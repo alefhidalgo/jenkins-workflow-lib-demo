ef4b783b57cfd9c929a68ed5fd4311327af52bfa
AOHG <alejandro.fernandez@gft.com>
2022-11-28 19:08:21 +0100

    test

--- vars/stage.groovy
+++ vars/stage.groovy
@@ -1,9 +1,15 @@
+import groovy.inspect.swingui.AstNodeToScriptVisitor
+
+
 void call(String label, Closure body){
  
   steps.stage(label){
     echo "Custom Stage"
     echo "------Declared Methods----------"
-    println body.metaClass.classNode.getDeclaredMethods("doCall")[0].code.text
+     new StringWriter().with { writer ->
+      body.metaClass.classNode.getDeclaredMethods("doCall")[0].code.visit new AstNodeToScriptVisitor( writer )
+      println "{\n$writer}"
+    }
     echo "--------------------------------"
     body()
     }
