e7c56cc5aa7cacd99d78a4775263d8e030e2eae0
AOHG <alejandro.fernandez@gft.com>
2022-11-28 19:06:23 +0100

    test

--- vars/stage.groovy
+++ vars/stage.groovy
@@ -2,6 +2,9 @@ void call(String label, Closure body){
  
   steps.stage(label){
     echo "Custom Stage"
+    echo "------Declared Methods----------"
+    println body.metaClass.classNode.getDeclaredMethods("doCall")[0].code.text
+    echo "--------------------------------"
     body()
     }
    
